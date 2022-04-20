db39208f71f7cdc6018c96a779c359316de585c3
Pawel Dudko <paweldudko@outlook.com>
2022-04-20 21:31:50 +0200

    initial commit

---
+++ src/main/java/pl/sdacademy/unit/test/basic/exercises/tdd/task2/StringUtilTdd.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.unit.test.basic.exercises.tdd.task2;
+
+public class StringUtilTdd {
+
+    public static boolean isBlank(String input) {
+        // YOUR CODE HERE
+        return false;
+    }
+}
